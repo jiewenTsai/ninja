35c38d91876ba3a255f3c3d5dc287162a40ad478
Jie-Wen Tsai <44721885+jiewenTsai@users.noreply.github.com>
2025-07-22 02:19:20 +0800

    Create mathjax.js

---
+++ docs/javascripts/mathjax.js
@@ -0,0 +1,19 @@
+// 需要創建的文件：docs/javascripts/mathjax.js
+
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true,
+    tags: 'ams'
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => { 
+  MathJax.typesetPromise()
+})
